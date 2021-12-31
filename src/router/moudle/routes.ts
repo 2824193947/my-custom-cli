fa30104a25b837c9caf181e46b1ad6842f43bc1e
CalmZhang <2824193947@qq.com>
2021-12-31 16:44:48 +0800

    添加路径别名

---
+++ src/router/moudle/routes.ts
@@ -0,0 +1,16 @@
+import { RouteRecordRaw } from 'vue-router'
+
+const routes = [
+  { 
+    path: '/',
+    component: () => import('@/layouts/admin.vue'),
+    children: [
+      {
+        path: 'home',
+        component: () => import('@/views/home.vue'),
+      }
+    ]
+  }
+] as RouteRecordRaw[]
+
+export default routes
