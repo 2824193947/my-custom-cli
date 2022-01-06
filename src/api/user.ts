5726c51088182e3390f59836dc2677f2d3be7e30
CalmZhang <2824193947@qq.com>
2022-01-06 18:01:51 +0800

    封装axios request方法

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,12 @@
 import { http } from '@/plugins/axios/index'
 
+interface User {
+  name: string
+  age: number
+  axm: '男' | '女'
+}
 export function user() {
-  return http.request({
+  return http.request<ResponseResult<User>>({
     url: '/user',
   })
 }
