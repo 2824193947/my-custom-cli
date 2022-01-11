38ea9e2da818cbcbeefa39c8e4bd03dc13bc6172
CalmZhang <2824193947@qq.com>
2022-01-11 14:18:04 +0800

    构helper。ts文件

--- src/router/autoloadRoutes/index.ts
+++ src/router/autoloadRoutes/index.ts
@@ -1,10 +1,10 @@
 import { RouteRecordRaw } from "vue-router"
-import { env } from '@/utils/index'
+import utils from '@/utils/index'
 import getRoutes from "./autoload"
 
 let routes = [] as RouteRecordRaw[]
 
-if (env.VITE_ROUTER_AUTOLOAD) {
+if (utils.env.VITE_ROUTER_AUTOLOAD) {
   routes = getRoutes()
 }
 
